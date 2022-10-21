b109cf0070dcdb714e68b6d6dbdcdde68bc463c8
zastar23 <florin.porut@gmail.com>
2022-10-21 22:42:58 +0300

    Design work

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-const navBtn = document.querySelector('.square > img')
+const navBtnImg = document.querySelector('.square > img')
+const square = document.querySelector('.square')
 const nav = document.querySelector('.header-nav')
 const links = document.querySelectorAll('.nav-list-link')
 const desktopList = document.querySelector('.desktop-nav-list')
 const desktopLinks = document.querySelectorAll('.desktop-nav-list-link')
+const hero = document.querySelector('.hero')
 
-navBtn.addEventListener('click', () => {
+navBtnImg.addEventListener('click', () => {
 	nav.classList.toggle('slide-in-top')
 	nav.classList.toggle('slide-out-top')
 })
@@ -13,15 +15,31 @@ links.forEach((link) => {
 	link.addEventListener('click', () => {
 		nav.classList.toggle('slide-in-top')
 		nav.classList.toggle('slide-out-top')
+		square.classList.remove('rotate-square-clock')
 	})
 })
 
-desktopLinks.forEach((link) => {
-	link.addEventListener('click', () => {
-		if (link.innerHTML === 'Home') {
-			return
-		}
-		desktopList.style.display = 'none'
-		document.querySelector('.square').style.opacity = '1'
-	})
+square.addEventListener('click', () => {
+	square.classList.toggle('rotate-square-clock')
 })
+
+// intersection observer for toggling navigations
+
+let options = {
+	root: null,
+	treshold: 1,
+}
+
+const observer = new IntersectionObserver(function (entries, observer) {
+	entries.forEach((entry) => {
+		if (!entry.isIntersecting) {
+			desktopList.classList.add('scroll-change-hidden')
+			square.classList.add('scroll-change-visible')
+		} else {
+			desktopList.classList.remove('scroll-change-hidden')
+			square.classList.remove('scroll-change-visible')
+		}
+	})
+}, options)
+
+observer.observe(desktopList)
